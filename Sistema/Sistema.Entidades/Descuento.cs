44167eea950465e9cfab430abc90c0a55518e299
David28 <61263018+Darent28@users.noreply.github.com>
2022-11-15 23:45:20 -0600

    entidades

---
+++ Sistema/Sistema.Entidades/Descuento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Entidades
+{
+    public class Descuento
+    {
+        public string idDescuento { get; set; }
+        public string descuento_ { get; set; }
+        public string IdCodigo { get; set; }
+        public string fecha { get; set; }
+        public string fechaAux { get; set; }
+        public string Op { get; set; }
+    }
+}
---
+++ Sistema/Sistema.Entidades/Devoluciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Entidades
+{
+    public class Devoluciones
+    {
+        public int IDr { get; set; }
+        public int Id_D { get; set; }
+        public string codigo { get; set; }
+        public string motivo { get; set; }
+        public string merma { get; set; }
+
+    }
+}
