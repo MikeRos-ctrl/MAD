528e072cf853563bd49c388b056e895cc7ce7cd8
David28 <61263018+Darent28@users.noreply.github.com>
2022-11-16 03:39:18 -0600

    DAtos

--- Sistema/Sistema.Datos/D_Descuento.cs
+++ Sistema/Sistema.Datos/D_Descuento.cs
@@ -9,12 +9,39 @@ using System.Threading.Tasks;
 
 namespace Sistema.Datos
 {
-
-   
-    public class D_Departamento
+    public class D_Descuento
     {
+        public DataTable sp_Codigo_Vali(string idDescuento)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Connection.Get_Instancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("sp_Codigo_Vali", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@idDescuento", SqlDbType.VarChar).Value = idDescuento;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                return null;
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
 
-        public DataTable sp_Get_Departamentos()
+
+    
+
+        public DataTable sp_Get_CostoConDescuento()
         {
             SqlDataReader Resultado;
             DataTable Table = new DataTable();
@@ -23,7 +50,7 @@ namespace Sistema.Datos
             try
             {
                 SqlCon = Connection.Get_Instancia().CrearConexion();
-                SqlCommand Comando = new SqlCommand("sp_Get_Departamentos", SqlCon);
+                SqlCommand Comando = new SqlCommand("sp_Get_CostoConDescuento", SqlCon);
                 Comando.CommandType = CommandType.StoredProcedure;
                 SqlCon.Open();
                 Resultado = Comando.ExecuteReader();
@@ -42,20 +69,20 @@ namespace Sistema.Datos
 
 
 
-
-
-        public string sp_GestionarDepartamentos(Departamento Obj)
+        public string sp_GestionarDescuento(Descuento Obj)
         {
             string Rpta = "";
             SqlConnection SqlCon = new SqlConnection();
             try
             {
                 SqlCon = Connection.Get_Instancia().CrearConexion();
-                SqlCommand Comando = new SqlCommand("sp_GestionarDepartamentos", SqlCon);
+                SqlCommand Comando = new SqlCommand("sp_GestionarDescuento", SqlCon);
                 Comando.CommandType = CommandType.StoredProcedure;
-                Comando.Parameters.Add("@id", SqlDbType.Int).Value = Obj.id;
-                Comando.Parameters.Add("@registered_by", SqlDbType.VarChar).Value = Obj.registered_by;
-                Comando.Parameters.Add("@nombre", SqlDbType.VarChar).Value = Obj.nombre;
+                Comando.Parameters.Add("@idDescuento", SqlDbType.Int).Value = Obj.idDescuento;
+                Comando.Parameters.Add("@descuento_", SqlDbType.Float).Value = Obj.descuento_;
+                Comando.Parameters.Add("@IdCodigo", SqlDbType.VarChar).Value = Obj.IdCodigo;
+                Comando.Parameters.Add("@fecha", SqlDbType.Date).Value = Obj.fecha;
+                Comando.Parameters.Add("@fechaAux", SqlDbType.Date).Value = Obj.fechaAux;
                 Comando.Parameters.Add("@Op", SqlDbType.Char).Value = Obj.Op;
                 SqlCon.Open();
                 //Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo ingresar el registro";
